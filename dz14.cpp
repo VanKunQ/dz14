7023c1a6de8017c54ac4608e615fa98e582b3194
ivanan <Kudinov.vanya@bk.ru>
2021-10-02 15:05:47 +0300

    final1.2

--- dz14.cpp
+++ dz14.cpp
@@ -1,21 +1,31 @@
 ﻿
 #include <iostream>
 #include <string>
+#include <iomanip>
 int main()
 {
-	std::cout << "What is your name? \n";
+	
+	std::cout << "What is your name? ";
 	std::string name;
-	std::cin >> name;
+    std::cin >> name;
+	std::getline(std::cin, name);
+	
 
 
-	std::cout << "Are you a programmer? Yes or no. \n";
-	std::string yesorno;
-	std::cin >> yesorno;
+	std::cout << "Are you a programmer? Yes or no. ";
+	std::string YesorNo;
+	std::cin >> YesorNo;
+	std::getline(std::cin, YesorNo);
 
-	std::cout << "Good luck, you are not a programmer Ha-ha-ha \n";
+	std::cout << "Good luck, you are not a programmer Ha-ha-ha ";
 	
 	
-	std::cout << name << " " << yesorno << "\n";
+	std::cout << name << " " << YesorNo << "\n";
+
+	std::string first = "name";
+	std::cout << first.length() << "\n";
+	std::cout << first.front() << "\n";
+	std::cout << first.back() << "\n";
 
 	std::cin;
 	return 0;
